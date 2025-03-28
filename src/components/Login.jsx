c8d9a14b22b38bba92c53f8e01b50ca7b7713fe8
Avinabh Kumar <kumar.avi3112@gmail.com>
2025-03-28 14:25:32 +0530

    fixed import error and login functionality working

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,18 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { addUser } from "./utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { Base_URl } from "../utils/constants";
+import { addUser } from "../utils/userSlice";
 export const Login = () => {
   const [emailID, setEmailId] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:7777/login",
+        Base_URl + "/login",
         {
           emailID,
           password,
@@ -18,6 +21,7 @@ export const Login = () => {
       );
       // console.log(res.data);
       dispatch(addUser(res.data));
+      return navigate("/");
     } catch (error) {
       console.log(error);
     }
